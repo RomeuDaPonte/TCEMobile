0145fb563bfc386bfa7f2821f570ac1c78c6e3f6
RomeuDaPonte <57477053+RomeuDaPonte@users.noreply.github.com>
2020-06-05 16:39:19 +0100

    improved navigation; deleted unnecessary files

--- app/screems/Espenses/IndexScreen.js
+++ app/screems/Espenses/IndexScreen.js
@@ -1,40 +1,103 @@
-import React, { useRef } from "react";
+import React from "react";
+import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import ListOfExpenses from "./ListOfExpensesScreen";
 import Screen from "../../components/Screen";
-import AppIconNavigator from "../../components/AppIconNavigator";
 import NewExpense from "../Espenses/NewExpenseScreen";
 import NewUser from "../Espenses/NewUserScreen";
 import Search from "../Espenses/SearchScreen";
 import Settings from "../Account/SettingsScreen";
+import colors from "../../LayoutHelpers/colors";
+import useToggleKeyboard from "../../customHooks/useToggleKeyboard";
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const IndexScreen = ({ route }) => {
-  const refContext = useRef(null);
-
+  const { showKeyboard } = useToggleKeyboard();
   // const { user } = route.params;
   // console.log(user);
 
   return (
     <Screen>
-      <NavigationContainer ref={refContext} independent={true}>
-        <Stack.Navigator
-          initialRouteName="ListOfExpenses"
-          screenOptions={{ headerShown: false }}
+      <NavigationContainer independent={true}>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName = "view-list";
+
+              if (route.name === "LisOfExpenses") iconName = "view-list";
+              else if (route.name === "NewUser") iconName = "account-plus";
+              else if (route.name === "Settings") iconName = "settings";
+              else if (route.name === "Search") iconName = "table-search";
+              else if (route.name === "NewExpense") iconName = "plus";
+
+              return (
+                <MaterialCommunityIcons
+                  style={styles.icon}
+                  name={iconName}
+                  size={30}
+                  color={color}
+                />
+              );
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: colors.primary,
+            inactiveTintColor: "gray",
+            style: [styles.nav, showKeyboard],
+          }}
         >
-          <Stack.Screen name="ListOfExpenses" component={ListOfExpenses} />
-          <Stack.Screen name="NewExpense" component={NewExpense} />
-          <Stack.Screen name="NewUser" component={NewUser} />
-          <Stack.Screen name="Search" component={Search} />
-          <Stack.Screen name="Settings" component={Settings} />
-        </Stack.Navigator>
-        <AppIconNavigator navigationContext={refContext} />
+          <Tab.Screen
+            name="ListOfExpenses"
+            component={ListOfExpenses}
+            options={{
+              tabBarLabel: "",
+            }}
+          />
+          <Tab.Screen
+            name="NewUser"
+            component={NewUser}
+            options={{
+              tabBarLabel: "",
+            }}
+          />
+          <Tab.Screen
+            name="Settings"
+            component={Settings}
+            options={{
+              tabBarLabel: "",
+            }}
+          />
+          <Tab.Screen
+            name="Search"
+            component={Search}
+            options={{
+              tabBarLabel: "",
+            }}
+          />
+          <Tab.Screen
+            name="NewExpense"
+            component={NewExpense}
+            options={{
+              tabBarLabel: "",
+            }}
+          />
+        </Tab.Navigator>
       </NavigationContainer>
     </Screen>
   );
 };
+
+const styles = StyleSheet.create({
+  icon: {
+    marginTop: 15,
+  },
+  nav: {
+    backgroundColor: colors.light,
+  },
+});
 
 export default IndexScreen;
