0eba0bbdd19c4dd062a8fe7b49f9f58cd485f921
RomeuDaPonte <57477053+RomeuDaPonte@users.noreply.github.com>
2020-07-09 10:46:17 +0100

    improved listitem behavior

--- app/screems/Espenses/IndexScreen.js
+++ app/screems/Espenses/IndexScreen.js
@@ -23,73 +23,71 @@ const IndexScreen = ({ route }) => {
   return (
     <ListOfExpensesProvider>
       <Header />
-      <Screen>
-        <NavigationContainer independent={true}>
-          <Tab.Navigator
-            screenOptions={({ route }) => ({
-              tabBarIcon: ({ focused, color, size }) => {
-                let iconName = "view-list";
+      <NavigationContainer independent={true}>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName = "view-list";
 
-                if (route.name === "LisOfExpenses") iconName = "view-list";
-                else if (route.name === "NewUser") iconName = "account-plus";
-                else if (route.name === "Settings") iconName = "settings";
-                else if (route.name === "Search") iconName = "table-search";
-                else if (route.name === "NewExpense") iconName = "plus";
+              if (route.name === "LisOfExpenses") iconName = "view-list";
+              else if (route.name === "NewUser") iconName = "account-plus";
+              else if (route.name === "Settings") iconName = "settings";
+              else if (route.name === "Search") iconName = "table-search";
+              else if (route.name === "NewExpense") iconName = "plus";
 
-                return (
-                  <MaterialCommunityIcons
-                    style={styles.icon}
-                    name={iconName}
-                    size={30}
-                    color={color}
-                  />
-                );
-              },
-            })}
-            tabBarOptions={{
-              activeTintColor: colors.primary,
-              inactiveTintColor: "gray",
-              style: [styles.nav, showKeyboard],
+              return (
+                <MaterialCommunityIcons
+                  style={styles.icon}
+                  name={iconName}
+                  size={30}
+                  color={color}
+                />
+              );
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: colors.primary,
+            inactiveTintColor: "gray",
+            style: [styles.nav, showKeyboard],
+          }}
+        >
+          <Tab.Screen
+            name="ListOfExpenses"
+            component={ListOfExpenses}
+            options={{
+              tabBarLabel: "",
             }}
-          >
-            <Tab.Screen
-              name="ListOfExpenses"
-              component={ListOfExpenses}
-              options={{
-                tabBarLabel: "",
-              }}
-            />
-            <Tab.Screen
-              name="NewUser"
-              component={NewUser}
-              options={{
-                tabBarLabel: "",
-              }}
-            />
-            <Tab.Screen
-              name="Settings"
-              component={Settings}
-              options={{
-                tabBarLabel: "",
-              }}
-            />
-            <Tab.Screen
-              name="Search"
-              component={Search}
-              options={{
-                tabBarLabel: "",
-              }}
-            />
-            <Tab.Screen
-              name="NewExpense"
-              component={NewExpense}
-              options={{
-                tabBarLabel: "",
-              }}
-            />
-          </Tab.Navigator>
-        </NavigationContainer>
-      </Screen>
+          />
+          <Tab.Screen
+            name="NewUser"
+            component={NewUser}
+            options={{
+              tabBarLabel: "",
+            }}
+          />
+          <Tab.Screen
+            name="Settings"
+            component={Settings}
+            options={{
+              tabBarLabel: "",
+            }}
+          />
+          <Tab.Screen
+            name="Search"
+            component={Search}
+            options={{
+              tabBarLabel: "",
+            }}
+          />
+          <Tab.Screen
+            name="NewExpense"
+            component={NewExpense}
+            options={{
+              tabBarLabel: "",
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
     </ListOfExpensesProvider>
   );
 };
--- app/screems/Espenses/ListOfExpensesScreen.js
+++ app/screems/Espenses/ListOfExpensesScreen.js
@@ -5,46 +5,39 @@ import ListItem from "../../components/ListItem";
 import ListItemSeparator from "../../components/ListItemSeparator";
 import { useListOfExpensesValue } from "../../contexts/listOfExpensesContext";
 import ListItemDateSeparator from "../../components/ListItemDateSeparator";
+import ListItemDeleteAction from "../../components/ListItemDeleteAction";
 
 const ListOfExpenses = () => {
   const { listOfExpenses } = useListOfExpensesValue();
 
-  function findIndex(currentExpense) {
-    let elementIndex;
-    listOfExpenses.forEach((element, index) => {
-      if (currentExpense.leadingItem.date === element.date)
-        return (elementIndex = index);
-    });
-    return elementIndex;
-  }
-
   function renderSeparator(currentExpense) {
-    const elementIndex = findIndex(currentExpense);
-    if (
-      listOfExpenses[elementIndex + 1].date.slice(0, 10) ===
-      listOfExpenses[elementIndex].date.slice(0, 10)
-    )
-      return <ListItemSeparator />;
-    else
-      return (
-        <ListItemDateSeparator date={listOfExpenses[elementIndex + 1].date} />
-      );
+    for (let i = 0; i <= listOfExpenses.length; i++) {
+      if (listOfExpenses[i]._id === currentExpense.leadingItem._id) {
+        if (listOfExpenses[i + 1].date) {
+          if (listOfExpenses[i + 1].date.slice(0, 10) === listOfExpenses[i].date.slice(0, 10))
+            return <ListItemSeparator />
+          else return <ListItemDateSeparator date={listOfExpenses[i + 1].date} />
+        }
+      }
+    }
+    return <ListItemSeparator />
   }
 
   return (
     <>
       {listOfExpenses && (
         <>
-          <ListItemDateSeparator
-            date={listOfExpenses[0].date}
-            otherStyles={{ paddingTop: 20 }}
-          />
+          {listOfExpenses.length > 1 && (
+            <ListItemDateSeparator
+              date={listOfExpenses[0].date}
+              otherStyles={{ paddingTop: 20 }}
+            />
+          )}
           <FlatList
             data={listOfExpenses}
             keyExtractor={(expense) => expense._id}
-            renderItem={({ item }) => <ListItem item={item} />}
+            renderItem={({ item }) => <ListItem item={item} renderRightActions={() => <ListItemDeleteAction onPress={() => console.log("deleted")} />} />}
             ItemSeparatorComponent={renderSeparator}
-            refreshing={true}
           />
         </>
       )}
